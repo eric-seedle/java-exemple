33f3f7e44b16fd8900261fc5237071ca2f085ffa
REYNIER <66683621+eric-seedle@users.noreply.github.com>
2020-08-10 13:08:32 +0200

    Update JavaExemple.java
    Nouvelle modification pour Jenkins

--- JavaExemple.java
+++ JavaExemple.java
@@ -3,9 +3,10 @@ public class JavaExemple{
 	public static void main(String[] args){
 		System.out.println("Hello World");
 		System.out.println("il devient logique que cela fonctionne");
+		test();
 	}
 	
-	public static void Test(){
+	public static void test(){
 		System.out.println("Méthode non utilisée");	
 	}
 }
